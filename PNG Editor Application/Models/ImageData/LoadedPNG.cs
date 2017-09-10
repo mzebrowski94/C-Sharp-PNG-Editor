9b2d0c74f87549503cfca1f33fc4cd4036e4ef51
mzebrowski94 <mzebrowski94@gmail.com>
2017-09-10 15:30:11 +0200

    Creating model classes

---
+++ PNG Editor Application/Models/ImageData/LoadedPNG.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PNG_Editor_Application.Models.ImageData
+{
+    /// <summary>
+    /// Contains decoded png raw data.
+    /// </summary>
+    class LoadedPNG
+    {
+
+    }
+}
